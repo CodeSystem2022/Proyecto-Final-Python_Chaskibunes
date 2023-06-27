40121d412fdc0cd4ddca8bbd20a090b22c9e2fce
Valentin Albornoz <valeg486@gmail.com>
2023-06-27 00:26:50 -0300

    Proyecto Integrador Python
    Commitemos proyecto Integrador Python con el framework django y la conección a la base de datos

---
+++ Project_integrador_python/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Project_integrador_pythonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Project_integrador_python'
