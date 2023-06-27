010049b8f5ce88614571b2bfb45feda0af226337
Ale Maugouber <alemago14@yahoo.com>
2023-06-27 12:59:41 -0300

    Agregado de models

---
+++ python_app/models.py
@@ -0,0 +1,10 @@
+class Product(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad_stock = models.IntegerField()
+
+    class Meta:
+        db_table = 'productos'
+
+    def _str_(self):
+        return f"{self.nombre} - Precio: {self.precio} - Cantidad en stock: {self.cantidad_stock}"
